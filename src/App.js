af14b92885674a7d708fa10ba5c327afd7a5df51
Colman Currie <curriecolman@gmail.com>
2020-05-19 10:33:17 -0700

    add browser-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/HomePage'; 
+import {
+  BrowserRouter as Browser,
+  Route
+} from 'react-router-dom'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <HomePage/>
+      <Route/>
     </div>
+    </Router>
   );
 }
 
