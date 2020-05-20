f5fcddf8e51f731bb723b0aa9bbc3402dbb3e894
Colman Currie <curriecolman@gmail.com>
2020-05-19 22:38:09 -0700

    add component CommentList

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,25 +1,32 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
+import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
 
 const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
-    const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: []});
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
 
     useEffect(() => {
-        setArticleInfo({ upvotes: 3})
-    });
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            console.log(body);
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
 
-    if (!article) return <h1>Article does not exist!</h1>
+    if (!article) return <NotFoundPage />
 
     const otherArticles = articleContent.filter(article => article.name !== name);
 
     return (
         <>
         <h1>{article.title}</h1>
-         <p>{articleInfo.upvotes} Upvotes! </p>
+        <p>This post has been upvoted {articleInfo.upvotes} times</p>
         {article.content.map((paragraph, key) => (
             <p key={key}>{paragraph}</p>
         ))}
