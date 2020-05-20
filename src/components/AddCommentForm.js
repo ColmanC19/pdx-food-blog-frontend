c45fc780c574798c340c42f7be45e5e9706e501f
Colman Currie <curriecolman@gmail.com>
2020-05-19 23:17:49 -0700

    add comment form with styling

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const AddCommentForm = () => {
-    
+    <div className="add-comment-form">
+      <input type="text" />
+      <textarea rows="4" cols="50" />
+      <button>Submit Comment</button>
+    </div>
+
 }
 
 export default AddCommentForm;
