db636ee43814a98eb5b7d60a06a82457eb6f46d1
Colman Currie <curriecolman@gmail.com>
2020-05-19 23:48:17 -0700

    addcommentform is fleshed out and ready to use

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const AddCommentForm = () => {
+const AddCommentForm = ({ articleName, setArticleInfo}) => {
+    const [username, setUsername] = useState('');
+    const [commentText, setCommentText] = useState('');
+    const addComment = async () => {
+        const result = await fetch(`/api/articles/${articleName}/add-comment/`, {
+            method: 'post',
+            body: JSON.stringify({username, text: commentText}),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+
+        });
+        const body = await result.json();
+        setArticleInfo(body);
+    }
+    return(
     <div className="add-comment-form">
-      <input type="text" />
-      <textarea rows="4" cols="50" />
-      <button>Submit Comment</button>
+        <h3>Comment Here</h3>
+        <label>
+            name:
+            <input type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
+        </label>
+        <label>
+            comment:
+            <textarea rows="4" cols="50" value={commentText} onChange={(event) => setCommentText(event.target.value)} />
+        </label>
+      <button onClick={() => addComment()}>Submit Comment</button>
     </div>
-
+);
 }
 
 export default AddCommentForm;
