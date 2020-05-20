f271717f61f3655c740a5cf0625aeaa89277e3e3
Colman Currie <curriecolman@gmail.com>
2020-05-20 16:06:08 -0700

    add image to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HomePage = () => (
     <>
     <h1>Portland Food Blog</h1>
-    <img style={{size: "50%"}} src= "https://pbs.twimg.com/media/DY2bN3VWkAYBdXC.jpg" alt="gathering"/>
+    <img style={{maxWidth: "100%"}} src= "https://pbs.twimg.com/media/DY2bN3VWkAYBdXC.jpg" alt="gathering"/>
     </>
     
 );
