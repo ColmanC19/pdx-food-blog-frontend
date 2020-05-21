198d287fea27abfceb332a60fb3c784b5c8cb93a
Colman Currie <curriecolman@gmail.com>
2020-05-20 17:20:48 -0700

    add info for food carts

--- src/pages/article-content.js
+++ src/pages/article-content.js
@@ -27,17 +27,8 @@ const articles = [
         name: 'criminally-underrated',
         title: "Hidden Gems that don't get the credit they deserve",
         content: [
-            `Here are some of the hottest food carts in town
-            1) Merendero Estrela
-            7129 SE 82nd Ave 
-            Open 11:30A-10P Daily`,
-        ]
-    },     {
-        name: 'cart-wars',
-        title: 'Best Food Carts in PDX',
-        content: [
-            `Today is the day I talk about something which scares most people: resumes.
-            In reality, I'm not sure why people have such a hard time with proin congue
+            `It's a strange world we live in today. Here's my guide to take out, tipping 
+            and everything in between. As proin congue dui in facilisis aliquet
             ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
             Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
             non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
@@ -54,7 +45,45 @@ const articles = [
             nisi. Nunc vel pulvinar diam. Nam eleifend egestas viverra. Donec finibus lectus 
             sed lorem ultricies, eget ornare leo luctus. Morbi vehicula, nulla eu tempor 
             interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
-            consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
+            consequat quam. Vivamus accumsan.`,
+
+        ]
+    },     {
+        name: 'cart-wars',
+        title: 'Best Food Carts in PDX',
+        content: [
+            `Here are some of the hottest food carts in town`,
+            `1) Merendero Estrela
+            7129 SE 82nd Ave 
+            Open 11:30A-10P Daily`, 
+            `Their authentic, 
+            homemade baleadas are unparralelled. Soft, chewy. 
+            Better than a torta or gasp... a burrito! Find out for yourself ASAP`,
+            `2) MF Tasty`,
+            `3927 N Williams Ave Open 11:30A-8:30P Fri-Mon`,
+            `Lign up for their fresh seasonal and 
+            often times downright sinister meals. Crunchwraps done correctly,
+            riffs of a Big Mac and paella galore`,
+            `3) Korean Twist`,
+            `5th & Oak downtown Open 11A-5P Mon-Sat`,
+            `Korean BBQ so good it'll make you upset at what you thought
+            K-BBQ was before. In a bowl over rice, in taco or burrito form, 
+            whatever you do, don't sleep on the yakisoba burrito`,
+            `4) JoJo PDX`,
+            `3582 SE Powell Blvd Open 11A-7P Daily`,
+            `Fried chicken sandwhiches and Jojos galore. But if you 
+            don't go more than once you'll miss out on the ever evolving 
+            specials and other sandwhiches like the spicy cheeseburger or
+            fried brussels sprout melt. Thank me later.`,
+            `5) Pablano Pepper`,
+            `6035 NE Halsey St Open 12P-10P Daily`,
+            `Fresh, veggie-mex food is in right now. And nowhere is it 
+            beter than here. Enchiladas with roasted mushrooms, tacos with
+            sweet potato and fried shallots. And if it's too spicy
+            it's conveniently located in the Baerlic beer pod 
+            where you can find some suds to wash it all down.`,
+            `Written by: Colman Currie`,
+
         ]
     },  
 ];
