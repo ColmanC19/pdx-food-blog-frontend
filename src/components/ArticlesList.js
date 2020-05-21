355a6d817903be7f1758f80f55588f2c10c9b55f
Colman Currie <curriecolman@gmail.com>
2020-05-20 17:55:19 -0700

    add in filler content

--- src/components/ArticlesList.js
+++ src/components/ArticlesList.js
@@ -6,7 +6,7 @@ const ArticlesList = ({ articles }) => (
     {articles.map((article, key) => (
         <Link className="article-list-item" key={key} to={`/article/${article.name}`}>
             <h3>{article.title}</h3>
-            <p>{article.content[0].substring(0, 150)}...</p>
+            <p>{article.content[0].substring(0, 48)}...</p>
         </Link>
     ))}
     </>
